df43b102b6957c0cdb6850aabcd3acf5620cd46a
Timothee Legros <timothee@ualberta.ca>
2022-09-23 18:04:26 -0600

    base functionality

--- freetests.py
+++ freetests.py
@@ -20,37 +20,38 @@ import unittest
 
 BASEURL = "http://127.0.0.1:8080"
 
+
 class TestYourWebserver(unittest.TestCase):
-    def setUp(self,baseurl=BASEURL):
+    def setUp(self, baseurl=BASEURL):
         """do nothing"""
         self.baseurl = baseurl
 
     def test_css(self):
         url = self.baseurl + "/base.css"
         req = request.urlopen(url, None, 3)
-        self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
-        self.assertTrue( req.info().get_content_type() == "text/css", ("Bad mimetype for css! %s" % req.info().get_content_type()))
+        self.assertTrue(req.getcode() == 200, "200 OK Not FOUND!")
+        self.assertTrue(req.info().get_content_type() == "text/css",
+                        ("Bad mimetype for css! %s" % req.info().get_content_type()))
 
     def test_get_root(self):
         url = self.baseurl + "/"
         req = request.urlopen(url, None, 3)
-        self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
+        self.assertTrue(req.getcode() == 200, "200 OK Not FOUND!")
 
     def test_get_indexhtml(self):
         url = self.baseurl + "/index.html"
         req = request.urlopen(url, None, 3)
-        self.assertTrue( req.getcode()  == 200 , "200 OK Not FOUND!")
-
+        self.assertTrue(req.getcode() == 200, "200 OK Not FOUND!")
 
     def test_get_404(self):
         url = self.baseurl + "/do-not-implement-this-page-it-is-not-found"
         try:
             req = request.urlopen(url, None, 3)
-            self.assertTrue( False, "Should have thrown an HTTP Error!")
+            self.assertTrue(False, "Should have thrown an HTTP Error!")
         except request.HTTPError as e:
-            self.assertTrue( e.getcode()  == 404 , ("404 Not FOUND! %d" % e.getcode()))
+            self.assertTrue(e.getcode() == 404, ("404 Not FOUND! %d" % e.getcode()))
         else:
-            self.assertTrue( False, "Another Error was thrown!")
+            self.assertTrue(False, "Another Error was thrown!")
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 #  coding: utf-8 
 import socketserver
+from pathlib import Path
+from os.path import abspath
+
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -16,8 +19,8 @@ import socketserver
 # limitations under the License.
 #
 #
-# Furthermore it is derived from the Python documentation examples thus
-# some of the code is Copyright © 2001-2013 Python Software
+# Furthermore, it is derived from the Python documentation examples thus
+# some code is Copyright © 2001-2013 Python Software
 # Foundation; All Rights Reserved
 #
 # http://docs.python.org/2/library/socketserver.html
@@ -26,13 +29,93 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# variable used to determine whether a requested file is actually within the local www directory
+SERVE_FILES_PATH = abspath('./www')
+
+class ResponseObject:
+    def __init__(self, protocol, status_code, content=None, file_path=None):
+        self.protocol = protocol
+        self.status_code = status_code
+        self.content = content
+        self.expires = 'Expires: -1\n'
+        self.cache = 'Cache-Control: private, max-age=0\n'
+        self.connection = 'Connection: close\n\n'
+
+        if file_path and file_path.endswith('.html'):
+            self.content_type = 'Content-type: text/html; charset=UTF-8\n'
+        elif file_path and file_path.endswith('.css'):
+            self.content_type = 'Content-type: text/css; charset=UTF-8'
+        else:
+            self.content_type = 'Content-type: text/plain; charset=UTF-8'
+
+        self.headers = self.expires + self.cache + self.content_type + self.connection
+
+        if status_code == '200':
+            self.formatted_response = protocol + ' 200 ' + 'OK\n' + self.headers + content
+        elif status_code == '404':
+            self.formatted_response = protocol + ' 404 ' + 'Not Found\n' + self.headers
+        elif status_code == '405':
+            self.formatted_response = protocol + ' 405 ' + 'Method Not Allowed\n' + self.headers
+        elif status_code == '301':
+            pass
+        else:
+            raise "Invalid status code"
+
+    @property
+    def response(self):
+        return self.formatted_response
+
+    @property
+    def response_bytes(self):
+        return bytearray(self.formatted_response.encode())
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        self.data = self.request.recv(1024).strip().decode('utf-8').replace("\r", "")
+
+        print("Request received:\n", self.data, "\n", sep="")
+
+        headers = self.data.split('\n')
+        base = headers[0].split(" ")
+        req_type = base[0]
+        file = base[1]
+        protocol = base[2]
+
+        # return 405 if request is not of type GET
+        if req_type != 'GET':
+            response = protocol + ' 405 ' + 'Method Not Allowed\n\n'
+            self.request.sendall(bytearray(response, "utf-8"))
+
+        if file == '/':
+            with open('www/index.html') as f:
+                content = f.read()
+                response = ResponseObject(protocol, '200', content)
+                print("Response:\n", response.response, "\n", sep="")
+                self.request.sendall(response.response_bytes)
+        else:
+            # prevent serving any files outside the local www directory this would trigger for a request where file
+            # escapes the folder e.g. ../../../something/something
+            if SERVE_FILES_PATH not in abspath('./www' + file):
+                response = ResponseObject(protocol, '404')
+                self.request.sendall(response.response_bytes)
+                return
+
+            # checks if the given path actually exists within the www directory
+            path = Path('www/' + file)
+            if not path:
+                response = ResponseObject(protocol, '404')
+                self.request.sendall(response.response_bytes)
+                return
+
+            # at this point we know the file exists, and we want to serve it
+            with open(path) as f:
+                content = f.read()
+                response = ResponseObject(protocol, '200', content, file)
+                print("Response:\n", response.response, "\n", sep="")
+                self.request.sendall(response.response_bytes)
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
